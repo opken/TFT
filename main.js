c70377d299ca00a10a4a05e4f2ff138b7f6becf1
opken <daika5doc@gmail.com>
2021-09-17 15:05:12 -0700

    update 2 weeks

--- main.js
+++ main.js
@@ -4,18 +4,164 @@ const port = 80
 const host = 'localhost'
 const cors = require('cors')
 
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+let champcost1 = []
+let champcost2 = []
+let champcost3 = []
+let champcost4 = []
+let champcost5 = []
+function fillChamps(data) {
+    for (let champ of data) {
+        switch(champ.cost) {
+            case 1:
+                for (let i = 0; i < 29; i++) {
+                    champcost1.push(champ.name)
+                }
+                break;
+            case 2:
+                for (let i = 0; i < 22; i++) {
+                    champcost2.push(champ.name)
+                }
+                break;
+            case 3:
+                for (let i = 0; i < 18; i++) {
+                    champcost3.push(champ.name)
+                }
+                break;
+            case 4:
+                for (let i = 0; i < 12; i++) {
+                    champcost4.push(champ.name)
+                }
+                break;
+            case 5:
+                for (let i = 0; i < 10; i++) {
+                    champcost5.push(champ.name)
+                }
+        }
+    }
+    shuffleChamps()
+}
+function shuffleChamps() {
+    champcost1 = shuffle(champcost1);
+    champcost2 = shuffle(champcost2);
+    champcost3 = shuffle(champcost3);
+    champcost4 = shuffle(champcost4);
+    champcost5 = shuffle(champcost5);
+}
 app.use(cors())
 app.get('/champions', (req,res) => {
     console.log('hi')
     data = require('./api/champions.json')
+    fillChamps(data)
     res.send(data)
 })
+app.get('/traits', (req,res) => {
+    console.log('hi')
+    data = require('./api/traits.json')
+    res.send(data)
+})
+app.get('/shop', (req,res) => {
+    let shop = []
+    for (let i = 0; i < 5; i ++) {
+        shop.push(getChamp())
+    }
+    console.log(shop)
+    res.send(shop)
+})
+app.post('/buy', (req,res) => {
+    console.log(req.body)
+})
+function getChamp(){
+    let champLevel = getChampLevel()
+    let champ;
+    switch(champLevel) {
+        case 1:
+            champ = champcost1[0]
+            break;
+        case 2:
+            champ = champcost2[0]
+            break;
+        case 3:
+            champ = champcost3[0]
+            break;
+        case 4:
+            champ = champcost4[0]
+            break;
+        case 5:
+            champ = champcost5[0]
+    }
+    shuffleChamps()
+    return champ
+}
+
+function shuffle(array) {
+    let current = array.length, temporary, random;
+
+    while(0 !== current) {
+        random = Math.floor(Math.random()*current)
+        current -= 1;
+
+        temporary = array[current]
+        array[current] = array[random]
+        array[random] = temporary
+    }
+    return array
+}
+function getChampLevel() {
+    let roll = getRandomInt(100);
+    let levelProbability = getProbability(9);
+    console.log(roll)
+    console.log(levelProbability)
+    for (let i = 0; i < 5; i++) {
+        if (roll >= levelProbability[i][0] && roll <= levelProbability[i][1]) {
+            switch(i) {
+                case 0:
+                    return 1
+                case 1:
+                    return 2
+                case 2:
+                    return 3
+                case 3:
+                    return 4
+                case 4:
+                    return 5    
+            }
+        }
+    }
+ }
+function getProbability(level) {
+    switch(level) {
+        case 1:
+            return [[0,99],0,0,0,0] 
+        case 2:
+            return [[0,99],0,0,0,0]
+        case 3:
+            return [[0,74],[75,99],0,0,0]
+        case 4:
+            return [[0,54],[55,84],[85,99],0,0]
+        case 5:
+            return [[0,44],[45,77],[78,97],[98,99],0]
+        case 6:
+            return [[0,24],[25,64],[65,94],[95,99],0]
+        case 7:
+            return [[0,18],[19,48],[49,83],[84,98],[99,99]]
+        case 8:
+            return [[0,14],[15,34],[35,69],[70,94],[95,99]]
+        case 9:
+            return [[0,9],[10,24],[25,54],[55,84],[85,99]]
+    }
+}
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
 
 app.get('/', (req, res) => {
     res.send('hi')
     console.log('ie')
 })
-
 app.use(express.static('static'));
 app.listen(port, host, () => {
     console.log(`Server is running at ${host}:${port}`)
--- static/client.js
+++ static/client.js
@@ -1,59 +1,267 @@
-function main() {
+let champJSONList = []
+let currentShop = []
+let champsOnBoard = []
+let currentTraits = {}
+let traitJSONList = []
+function getChampJSON() {
     $.ajax({
         method: 'GET',
         url: '/champions',
         contentType: 'application/json',
     }).done((result) => {
-        for (let i of result) {
-            createChamp(i.name, i.championId, i.cost, i.traits)
-        }
-    //    createChamp(result[1].name, result[1].championid, result[1].cost, result[1].traits)
-    //    createTraits(result[1].traits)
+        champJSONList = result
+        console.log(result)
     })
 }
+function getTraitJSON() {
+    $.ajax({
+        method: 'GET',
+        url: '/traits',
+        contentType: 'application/json',
+    }).done((result) => {
+        traitJSONList = result
+        console.log(traitJSONList)
+    })
+}
+function createShop() {
+    $.ajax({
+        method: 'GET',
+        url: '/shop',   
+        contentType: 'application/json',
+    }).done((result) => {
+        console.log(result)
+        currentShop = result
+        clearShop()
+        for (let i = 0; i < 5; i++) {
+            let champInfo = findChampInfo(result[i])
+            createChamp(champInfo.name, champInfo.championId, champInfo.cost, champInfo.traits, i)
+        }
+       
+    })
+}
+function findChampInfo(name) {
+    let champ = champJSONList.filter((champ) => {
+        return champ.name == name
+    })
+    return champ[0]
+}
+function clearShop() {
+    $('#champ0').empty();
+    $('#champ1').empty();
+    $('#champ2').empty();
+    $('#champ3').empty();
+    $('#champ4').empty();
+}
+function isEmpty(element) {
+    for (let i of element) {
+        if (i.innerHTML=="")
+            return true
+    }
+    return false
+}
+function createBoard() {
+    let board = $('#board')
+    for (let x = 0; x < 4; x++) {
+        let row = $('<div></div>')
+        for (let y = 0; y < 7; y++) {
+            let boardSpace = $('<div></div>').addClass('hexagon boardSpace')
+                .attr('ondrop', 'drop(event)')
+                .attr('ondragover', 'allowDrop(event)')
+
+            boardSpace.append($('<div></div>').addClass('hexagonInner'))
+            row.append(boardSpace)
+        }
+        if (x%2==1) {
+            row.addClass('oddRow')
+        }
+        board.append(row)
+    }
+}
+function buy(event) {
+    let boughtChamp, content
+
+    if (event.currentTarget.children[0]) {
+        boughtChamp = event.currentTarget.children[0].id
+        content = {name:boughtChamp} 
+        let champPicture = event.currentTarget.getElementsByTagName('img')[0].src
+        let bench = $('.bench')
+        if (isEmpty(bench)) {
+            event.currentTarget.children[0].remove()
+
+        }
+        for (let i of bench) {
+            if (i.innerHTML == "") {
+                let image = new Image()
+                image.src = champPicture
+                console.log(champPicture)
+                image.setAttribute('draggable', 'true')
+                image.setAttribute('ondragstart', 'drag(event)')
+                image.id = boughtChamp
+                i.append(image)
+                break
+            }
+        } 
+    }
+    $.ajax({
+        method: 'POST',
+        url: '/buy',
+        data: JSON.stringify(content),
+        contentType: 'application/json',
+    }).done((result) => {
+        console.log(result)
+    })
+}
+function getSynergies() {
+    let board = document.getElementById('board')
+    // console.log(board)
+    let champs = board.getElementsByTagName('img')
+    for (let c of champs) {
+        if (!champsOnBoard.includes(c.id))
+            champsOnBoard.push(c.id)
+    }
+    for (let c of champsOnBoard) {
+        let traits = getChampTraits(c)
+        for (let t of traits) {
+            if (currentTraits[t]) {
+                if (!currentTraits[t].includes(c))
+                    currentTraits[t].push(c)
+            }
+            else {
+                currentTraits[t] = [c]
+            }
+        }
+    }
+    $('#synergies').empty()
+    let synergies = document.getElementById('synergies')
+    for (let trait in currentTraits) {
+        let traitName = trait
+        let traitCount = currentTraits[trait].length
+        let traitJSONObject = getTraitJSONObject(traitName)
+        
+        let style;
+        for (let i of traitJSONObject.sets) {
+            if (traitCount < i.min || i.min ==1) {
+                style = i.style
+                break
+            }
+        }
+        let iconBg
+        if (style == 'bronze') {
+            iconBg = 'gray'
+        }
+        else if (style =='silver') {
+            iconBg = '#90c0d1'
+        }
+        else if (style =='gold') {
+            iconBg = 'gold'
+        }
+        // console.log(traitJSONObject)
+        let styleMin = getTraitStyle(traitJSONObject, style).min
+        let image = new Image()
+        image.src = '/traits/'+traitName.substring(5).toLowerCase()+'.svg'
+        image.classList.add('synergies_icon')   
+        image.style.backgroundColor = iconBg
+
+        let t = $('<div></div>').addClass('syngergy_container')
+            .append(image)
+            .append($('<div></div>').addClass("synergy").text(traitName.substring(5))
+                .append($('<div></div>').addClass('traitCount').text(traitCount+' / '+styleMin))
+        )
 
 
-function createChamp(name,id,cost,traits) {
-    console.log(id)
-    let c = $('<div></div>').addClass('champ').attr('id', name.substring(0,3))
+
+
+        // let a = $('<div></div>').addClass('traits')
+        // for (let t of traits) {
+        //     a.append($('<div></div>')
+        //         .append(("<img class='trait_icon' src='"+ /traits/+t.substring(5).toLowerCase()+'.png' + "'></img>"))
+        //         .append($('<span></span>').text(t.substring(5)).addClass('trait_text'))
+        //     )
+        // }
+        $('#synergies').append(t)
+    }
+    champsOnBoard = []
+    currentTraits = []
+    // console.log(currentTraits)
+}
+function getTraitStyle(trait, style) {
+    let res = trait.sets.filter((i)=> 
+        i.style == style
+    )
+    return res[0]
+}
+function getTraitJSONObject(name) {
+    let trait = traitJSONList.filter((t)=>
+        t.key == name
+    )
+    return trait[0]
+}
+function getChampJSONObject(name) {
+    let champ = champJSONList.filter((c)=> 
+        c.name == name
+    )
+    return champ[0]
+}
+function getChampTraits(champ) {
+    return getChampJSONObject(champ).traits
+}
+function createChamp(name,id,cost,traits, champ) {
+    let c = $('<div></div>').addClass('champ').attr('id', name)
         .append($('<div></div>').addClass('bottom')
 
             .append($('<div></div>').addClass('name').text(name))
             .append($('<div></div>').addClass('cost').text(cost))
         )
-        // .append($('<div></div>').addClass('image')
-        //     .css('background-image', 'url(' + '/champions/'+id+'.png' + ')')
-        // )
-        .append(("<img src='"+ /champions/+id+'.png' + "'></img>"))
-        .append($('<div></div>').addClass('traits').attr('id', name.substring(0,3)+'_trait'))
+        .append($('<div></div>').addClass('image')
+            .append(("<img src='"+ /champions/+id+'.png' + "'></img>")))
 
-    $('#board').append(c)
+    let a = $('<div></div>').addClass('traits')
+    for (let t of traits) {
+        a.append($('<div></div>')
+            .append(("<img class='trait_icon' src='"+ /traits/+t.substring(5).toLowerCase()+'.png' + "'></img>"))
+            .append($('<span></span>').text(t.substring(5)).addClass('trait_text'))
+        )
+    }
+    c.append(a)
+    $('#champ'+champ).append(c)
 
-    createTraits(name.substring(0,3)+'_trait', traits, name.substring(0,3), cost)
+    if (cost == 1) 
+        c.css('background-color', 'gray')
+    else if (cost == 2 )
+        c.css('background-color', '#227a31')
+    else if (cost == 3 )
+        c.css('background-color', '#2e84ab')
+    else if (cost == 4 )
+        c.css('background-color', '#aa16ba')
+    else if (cost == 5 )
+        c.css('background-color', '#ffa200')
+
+    return c
 
 }
-function createTraits(trait,traits,name, cost) {
-    for (let t of traits) {
-        $('#'+trait)
-            .append($('<div></div>')
-                .append(("<img class='trait_icon' src='"+ /traits/+t.substring(5).toLowerCase()+'.png' + "'></img>"))
-                .append($('<span></span>').text(t.substring(5)).addClass('trait_text'))
-            )
-            // .append(("<img class='trait_icon' src='"+ /traits/+t.substring(5).toLowerCase()+'.png' + "'></img>"))
-            // .append($('<span></span>').text(t.substring(5)).addClass('trait_text'))
-            // .append($('<br>'))
-    }
-    if (cost == 1) 
-        $('#'+name).css('background-color', 'gray')
-    else if (cost == 2 )
-        $('#'+name).css('background-color', '#227a31')
-    else if (cost == 3 )
-        $('#'+name).css('background-color', '#2e84ab')
-    else if (cost == 4 )
-        $('#'+name).css('background-color', 'purple')
-    else if (cost == 5 )
-        $('#'+name).css('background-color', '#ffa200')
-    
-    
-   
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+  
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+    console.log(ev.target)
+
+}
+  
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.append(document.getElementById(data));
+    getSynergies();
+}
+function sell(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    let soldChamp = document.getElementById(data).id
+    document.getElementById(data).remove()
+    console.log(soldChamp)
+    console.log(currentTraits)
+    getSynergies();
 }
